5dcea6d2d17d3bfbf4656b2d68cbe6db60a9bde7
Diego Marquina <diego.marquina@shell.com>
2021-10-27 14:34:16 +0000

    Localized time, fixed resampling issues

--- REMIT_to_timeseries.py
+++ REMIT_to_timeseries.py
@@ -33,16 +33,17 @@ frame = pd.concat(li, axis=0, ignore_index=True)
 
 #%%
 mapcode='FR' # 'PT', 'ES', 'FR', 'FI', 'SE3', 'SE', 'EE', 'SE1', 'NO2', 'NL', 'NO', 'HU', 'NO4', 'CZ', 'GB', 'PL', 'NO5', 'RO', 'DE_50HzT', 'DE_Amprion', 'DE_AT_LU', 'CH', 'DE_TransnetBW', 'AT', 'DE_TenneT_GER', 'IT-CNORTH', 'BE', 'IT-NORTH', 'DK2', 'IE', 'NO3', 'IT-CSOUTH', 'DK', 'DK1', 'IE_SEM', 'GR', 'IT-Sicily', 'LV', 'NO1', 'IT-Sardinia', 'SE4', 'NIE', 'LT', 'SE2', 'SK', 'IT-Priolo', 'BG', 'IT', 'IT-Rossano', 'IT-Brindisi', 'IT-Foggia', 'RS', 'IT-SOUTH', 'MK', 'ME', 'SI', 'AL', 'DE_LU', 'IT-Calabria', 'XK'
-unitname='FESSENHEIM 2'
-areatype = 'CTA' #options are CTA or BZN
+unitname='PALUEL 2'
+areatype = ['CTA','BZN'] #options are CTA or BZN
 
 
 df_fr = frame.loc[frame.MapCode==mapcode,:].copy()
 df_unit = df_fr.loc[df_fr.UnitName.str.contains(unitname),:]
 df_unit_active = df_unit.loc[df_unit.Status=='Active',:]
 df_unit_active_latest = df_unit_active.loc[df_unit_active.groupby('MRID')['Version'].idxmax(),:]
-df_unit_a_l_CTA = df_unit_active_latest.loc[df_unit_active_latest.AreaTypeCode==areatype,:]
+df_unit_a_l_CTA = df_unit_active_latest.loc[df_unit_active_latest.AreaTypeCode.isin(areatype),:]
 df_unit_a_l_CTA.loc[:,['StartTS','EndTS','UpdateTime']] = df_unit_a_l_CTA.loc[:,['StartTS','EndTS','UpdateTime']].apply(pd.to_datetime).copy()
+df_unit_a_l_CTA.loc[:,['StartTS','EndTS','UpdateTime']] = df_unit_a_l_CTA.apply(lambda x: x[['StartTS','EndTS','UpdateTime']].apply(lambda y: y.tz_localize(x['TimeZone']).tz_convert('UTC')), axis=1)
 df_unit_a_l_CTA = df_unit_a_l_CTA.sort_values(['StartTS','EndTS'])
 df_unit_a_l_CTA = df_unit_a_l_CTA.loc[df_unit_a_l_CTA.EndTS < '2030-01-01']
 
@@ -55,21 +56,21 @@ for index, row in df_unit_a_l_CTA.iterrows():
     temp_df.loc[pd.to_datetime(row['EndTS']),:]= [row['AvailableCapacity'], i]
     temp_df.available = pd.to_numeric(temp_df.available)
     temp_df.iter = pd.to_numeric(temp_df.iter, downcast='integer')
-    temp_df = temp_df.resample('h').interpolate()
+    temp_df = temp_df.resample('1T').interpolate()
     df_ts = df_ts.append(temp_df)
     i+=1
     
 df_ts = df_ts.loc[~df_ts.index.duplicated(keep='last')]
-df_ts = df_ts.resample('h').fillna('backfill')
+df_ts = df_ts.resample('1T').mean()
 df_ts.available.fillna(df_unit_a_l_CTA.InstalledCapacity.max(), inplace=True)
-
+df_ts = df_ts.resample('h').mean()
 
 # %%
 # Initialize client
 eq = EnergyQuantified(api_key='0168d6-b068b6-f41124-7108d3')
 
 # Free-text search (filtering on attributes is also supported)
-curves = eq.metadata.curves(q='FR @Fessenheim-2 Nuclear Capacity Available MW REMIT')
+curves = eq.metadata.curves(q='FR @Paluel-2 Nuclear Capacity Available MW REMIT')
 
 # Load time series data
 curve = curves[0]
@@ -96,7 +97,7 @@ periodseries = eq.period_instances.latest(
 # Convert to Pandas data frame
 df_eq = periodseries.to_timeseries(time.Frequency.PT1H).to_dataframe()
 df_eq.columns=df_eq.columns.get_level_values(0)
-df_eq.index = df_eq.index.tz_convert(None)
+df_eq.index = df_eq.index.tz_convert('UTC')
 
 
 #%% plot
